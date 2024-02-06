c0ff70e020a82e1889fabeae3133c589fafcfe44
jakestolarsky <stolarczyk.jk@gmail.com>
2024-02-06 21:28:30 +0100

    app working with jinja2 templates

--- project/main.py
+++ project/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, responses
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-
-@app.get("/")
-async def get_app():
-    return {"message": "Hello world!"}
+@app.get("/", response_class=HTMLResponse)
+async def read_route(request: Request):
+    print(BASE_DIR)
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Jan!"})

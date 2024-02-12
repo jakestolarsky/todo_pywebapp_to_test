e756aa2d3d7c15b4dcab95d9c9f3d50feecb2b5b
jakestolarsky <stolarczyk.jk@gmail.com>
2024-02-12 21:08:11 +0100

    simplify test_task_schema.py

--- project/tests/unit/test_task_schema.py
+++ project/tests/unit/test_task_schema.py
@@ -3,24 +3,24 @@ from pydantic import ValidationError
 
 from app.schemas.task_schema import Task
 
-class TestSchema():
-    def test_default_values(self):
-        task = Task(name="Test task")
 
-        assert task.id is None
-        assert task.completed is False
+def test_default_values():
+    task = Task(name="Test task")
 
-    def test_custom_values(self):
-        task = Task(id=1, name="Test Task", completed=False)
+    assert task.id is None
+    assert task.completed is False
 
-        assert task.id == 1
-        assert task.name == "Test Task"
-        assert task.completed is False
+def test_custom_values():
+    task = Task(id=1, name="Test Task", completed=False)
 
-    def test_invalid_values(self):
-        with pytest.raises(ValidationError):
-            Task(name=123, completed=123)
+    assert task.id == 1
+    assert task.name == "Test Task"
+    assert task.completed is False
 
-    def test_empty_task(self):
-        with pytest.raises(ValidationError):
-            Task()
+def test_invalid_values():
+    with pytest.raises(ValidationError):
+        Task(name=123, completed=123)
+
+def test_empty_task():
+    with pytest.raises(ValidationError):
+        Task()
